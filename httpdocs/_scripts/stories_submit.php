ca3716db1357cc027b4f7e6dd8c781f75c29b920
Zac Conant <ZacConant@journeyanconant.gateway.pace.com>
2015-11-28 18:08:31 -0600

    Minor fixes, transferred stories to redesign as well

---
+++ httpdocs/_scripts/stories_submit.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width">
+
+		<meta name="description" content="Share a story about how your life has been changed">
+		<meta name="keywords" content="Stories Jesus Life Change Journey Church Norman OK">
+
+		<title>Stories - What's Your Story?</title>
+
+		<!-- Fonts -->
+		<link rel="stylesheet" type="text/css" href="//cloud.typography.com/6186432/688704/css/fonts.css" />
+
+		<!-- Font Icons -->
+		<link rel="stylesheet" href="/_css/font-awesome.css" type='text/css'>
+
+		<!-- Stylesheets -->
+		<link rel="stylesheet" href="/_css/stories/normalize.css">
+		<link rel="stylesheet" href="/_css/stories/base.css">
+		<link rel="stylesheet" href="/_css/stories/grid.css">
+		<link rel="stylesheet" href="/_css/stories/style.css">
+
+		<link rel='stylesheet' media='screen and (max-width: 995px)' href='/_css/stories/tablet.css' />
+		<link rel='stylesheet' media='screen and (max-width: 767px)' href='/_css/stories/mobile.css' />
+		<link rel='stylesheet' media='screen and (max-width: 479px)' href='/_css/stories/mobile-portrait.css' />
+	</head>
+	<body id="submit-body">
+		<header>
+			<a href="/stories"><div id="logo"><img src="/_img/logo_black.png" width="50"/></div></a>
+		</header>
+
+		<div id="header-filler"></div>
+
+    <div id="submit">
+      <div class="container">
+        <div class="grid_12">
+          <h1>Thank You For Sharing Your Story!</h1>
+        </div>
+      </div>
+
+      <div class="clear"></div>
+    </div>
+
+    <?php
+      define('DB_NAME', 'stories');
+      define('DB_USER', 'root');
+      define('DB_PASS', 'root');
+      define('DB_HOST', 'localhost');
+
+      $con = mysql_connect(DB_HOST, DB_USER, DB_PASS);
+      if (!$con) {
+        die("Cannot connect to database" . mysql_error());
+      }
+
+      $db = mysql_select_db(DB_NAME, $con);
+      if (!$db) {
+        die("Cannot use " . DB_NAME . " " . mysql_error());
+      }
+
+      if(isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $find = $_POST['find'];
+        $story = $_POST['story'];
+        $email = $_POST['email'];
+      }
+
+      $sql = "INSERT INTO stories (Name, Find, Story, Email) VALUES ('$name', '$find', '$story', '$email')";
+
+      if(!mysql_query($sql)) {
+        die("Error " . mysql_error());
+      }
+
+      mysql_close($con);
+    ?>
+
+  </body>
+</html>
