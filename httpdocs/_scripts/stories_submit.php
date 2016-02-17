09fd9096ce555c1374f98a5535653fb2c1396257
Zac Conant <ZacConant@Journey-Zan-Conant.local>
2016-02-17 15:57:43 -0600

    Stories: Backend and Front end updates

--- httpdocs/_scripts/stories_submit.php
+++ httpdocs/_scripts/stories_submit.php
@@ -51,15 +51,7 @@
     </div>
 
     <?php
-      define('DB_NAME', 'stories');
-      define('DB_USER', 'storiesadmin');
-      define('DB_PASS', '64rR4t!s');
-      define('DB_HOST', 'localhost');
-
-      $con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-      if (!$con) {
-        die("Cannot connect to database" . mysqli_error());
-      }
+      require_once($_SERVER['DOCUMENT_ROOT'].'/_scripts/stories_connect.php');
 
       if(isset($_POST['submit'])) {
         $name = $_POST['name'];
