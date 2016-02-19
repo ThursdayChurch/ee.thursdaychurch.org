aab453b4cad4d15ec6b9c47afae45aa2e683c7b2
Zac Conant <ZacConant@Journey-Zan-Conant.local>
2016-02-19 16:57:01 -0600

    Storiesadmin: Story drag and sort finished

--- httpdocs/_scripts/stories_admin_update.php
+++ httpdocs/_scripts/stories_admin_update.php
@@ -4,13 +4,27 @@
 
     require_once($_SERVER['DOCUMENT_ROOT'].'/_scripts/stories_connect.php');
 
-    $result = mysqli_query($con, "SELECT * FROM stories ORDER BY 'Sorting Index'");
+    $result = mysqli_query($con, "SELECT ID, SortingIndex FROM stories ORDER BY SortingIndex");
+
+    $sql = "";
 
     while($row = mysqli_fetch_assoc($result)) {
-      
+      if(in_array($row['ID'], $ids, TRUE)) {
+        $id = intval($row['ID']);
+
+        $sortingIndex = intval($row['SortingIndex']);
+
+        $index = array_search($id, $ids);
+
+        if($index != $sortingIndex) {
+            $sql .= "UPDATE stories SET SortingIndex = $index WHERE ID = $id; ";
+        }
+      }
     }
 
-    mysqli_query($con, "UPDATE stories SET SortingIndex = 1 WHERE ID = $ids[0]");
+    mysqli_multi_query($con, $sql);
+
+    echo "Your changes have been saved";
 
     mysqli_close($con);
   }
