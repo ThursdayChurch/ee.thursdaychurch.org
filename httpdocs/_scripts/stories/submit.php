69df70af9ecc5fada2b3d1f0cf3c71e2c83fbada
Zac Conant <ZacConant@Journey-Zan-Conant.local>
2016-06-28 13:17:18 -0500

    Stories: email notification test sent

--- httpdocs/_scripts/stories/submit.php
+++ httpdocs/_scripts/stories/submit.php
@@ -28,7 +28,9 @@
       $connection = new StoriesConnection();
       $connection->submit($_POST["name"], $_POST["beginning"], $_POST["persevered"], $_POST["growth"], $_POST["email"]);
 
-			mail('zac.conant@journeychurch.tv', 'Submission Email Test' , 'Email Test');
+			$mail = mail('zac.conant@journeychurch.tv', 'Submission Email Test' , 'Email Test');
+
+			echo $mail;
     ?>
 
     <section id="submit" class="bg-img text-center">
